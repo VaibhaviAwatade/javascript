34f8966937d4811509b5b3e6d0bd2206688d496e
VaibhaviAwatade <vaibhaviawatade196@gmail.com>
2023-05-11 10:14:48 +0530

    Swapping   Number

---
+++ 02_swapVariable.js
@@ -0,0 +1,19 @@
+var valueOne="Sweety";
+var valueTwo="Cuite";
+console.log("Before Swap..","Value1",valueOne,"Value2",valueTwo);
+var temp=valueOne;
+valueOne=valueTwo;
+valueTwo=temp;
+
+console.log("After Swap..","Value1",valueOne,"Value2",valueTwo);
+
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before Swap",num1, num2 ,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("AfterSwap",num1,num2,num3);
---
+++ 03_funAssig01.js
@@ -0,0 +1,34 @@
+function nameShow(){
+    console.log("Nita");
+}
+nameShow();
+function ageDisplay(){
+    console.log("21");
+}
+ageDisplay();
+
+
+function personalDetails(firstName,lastName,collegeName){
+    console.log("My Name is",firstName , lastName);
+    console.log("College Name is",collegeName);
+}
+personalDetails("Nita" ,"Shrma","VMS");
+
+
+
+function swapValueDude(valueOne,valueTwo){
+console.log(valueOne,valueTwo);
+}
+swapValueDude("Virat","Anushka");
+swapValueDude("1000","2000");
+
+
+function addThreeValues(numOne,numTwo,numThree){
+    var Result;
+    numOne+numTwo+numThree;
+    console.log("Result is",Result);
+    console.log(numOne,numTwo,numThree);
+    
+}
+addThreeValues("10","600","40");
+addThreeValues("Hello","Good","Morning");
