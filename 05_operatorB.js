16a3b9a01303edc30d91e153606d13232ee80c5a
VaibhaviAwatade <vaibhaviawatade196@gmail.com>
2023-05-16 11:21:38 +0530

    Ternary Operator

---
+++ 05_operatorB.js
@@ -0,0 +1,45 @@
+var greateNumber=function(num1,num2){
+    var Result=num1>num2 ?`${num1} is greter `:`${num2} is greter`;
+    console.log(Result);
+}
+greateNumber(10,-10);
+greateNumber(800,899);
+
+
+
+var isEvenOrOddNum=function (num){
+    var result=num%2==0 ?true :false;
+    return result;
+
+}
+ var result=isEvenOrOddNum(29);
+var res=(result==true)?"29 is Even Number":"29 is Odd Number";
+console.log(`${res}`);
+
+var result=isEvenOrOddNum(44);
+var res=(result==true)?"44 is Even Number":"44 is Odd Number";
+console.log(`${res}`);
+
+var result=isEvenOrOddNum(0);
+var res=(result==true)?"0 is Even Number":"0 is Odd Number";
+console.log(`${res}`);
+
+var result=isEvenOrOddNum(101);
+var res=(result==true)?"101 is Even Number":"101 is Odd Number";
+console.log(`${res}`);
+
+
+var wordLength=function (word){
+    var len=word.length;
+    var result=len%2==0 ?"Even":"Odd";
+    return result;
+}
+var returnValue=wordLength("javaScript");
+console.log(`javaScript word length is ${returnValue}`);
+
+var returnValue=wordLength("developer");
+console.log(`developer word length is ${returnValue}`);
+
+var returnValue=wordLength("Google");
+console.log(`Google word length is ${returnValue}`);
+
---
+++ 05_operatorTernaryAssigC.js
@@ -0,0 +1,33 @@
+var boyName="Billgates";
+var age=27;
+var gender="male";
+
+var result =(gender=="male"&& age>=21) ?"Hey Billgate you are eligibile for Marriage":"Hey Billagete you are not eligibile for Marriage";
+console.log(`${result}`);
+
+var boyName="Stew Jobs";
+var age=17;
+var gender="male";
+
+var result =(gender=="male"&& age>=21) ?"Hey Stew Jobs you are eligibile for Marriage":"Hey Stew Jobs you are not eligibile for Marriage";
+console.log(`${result}`);
+
+console.log(`...........................`);
+
+
+var boyName="Malinda Gates";
+var age=27;
+var gender="Female";
+
+var result =(gender=="Female"&& age>=18) ?"Hey Malinda Gates you are eligibile for Marriage":"Hey Malinda Gates you are not eligibile for Marriage";
+console.log(`${result}`);
+
+
+var boyName="Jenifer";
+var age=16;
+var gender="Female";
+
+var result =(gender=="Female"&& age>=18) ?"Hey Jenifer you are eligibile for Marriage":"Hey Jenifer you are not eligibile for Marriage";
+console.log(`${result}`);
+
+
